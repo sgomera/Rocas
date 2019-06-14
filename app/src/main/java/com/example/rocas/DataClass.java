23cafa4aa849e552bf92e930c6ca977d58fcc974
sgomera <silviagomera@gmail.com>
2019-06-14 10:52:28 +0200

    New Menu item additional Data working

---
+++ app/src/main/java/com/example/rocas/DataClass.java
@@ -0,0 +1,13 @@
+package com.example.rocas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DataClass extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data);
+    }
+}
