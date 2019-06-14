f8369ef7971084613293e1821cf5b144b9a02e91
sgomera <silviagomera@gmail.com>
2019-06-14 12:08:07 +0200

    Toolbar

--- app/src/main/java/com/example/rocas/MainActivity.java
+++ app/src/main/java/com/example/rocas/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.rocas;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //toolbar
+        Toolbar toolbar = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+        }
+        setSupportActionBar(toolbar);
     }
 
     //creem un nou mètode pe a que quan s'apreti el botó de dades addicionals, s'executi
